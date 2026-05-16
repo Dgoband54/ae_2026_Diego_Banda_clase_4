9550d9cb96074f1a8828ce6af2a3f93d7d26da11
Diego Banda <diegobanda54@gmail.com>
2026-05-16 10:27:07 -0500

    clase 4-1

---
+++ main.kt
@@ -0,0 +1,18 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+    //siempre debo tipar mis variables (0 contante)
+    val mensaje = "hola mundo"
+    var variableMutable: Int = 5
+    println(mensaje)
+    println(variableMutable)
+    
+    
+    variableMutable = 10
+    println(variableMutable)
+    
+    mensaje = "Hola mundo mutado"
+    println(mensaje)
+}
